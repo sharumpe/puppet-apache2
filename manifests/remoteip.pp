0fc45270f62b147ec9f20198267ee3487a9771a4
sharumpe <nospam@macwebb.com>
2014-12-02 10:34:55 -0800

    normalizing the layout of the modules.

--- manifests/remoteip.pp
+++ manifests/remoteip.pp
@@ -10,17 +10,18 @@ define apache2::remoteip
 	# Enable the module
 	apache2::enableModule{ 'remoteip': }
 
-        # Pick the template path we're going to use
-        $mod_path = get_module_path('apache2')
-        $specific = "$mod_path/templates/$operatingsystem/$operatingsystemrelease$params::remoteIpConfigPath.erb"
-        $default  = "$mod_path/templates/default$params::remoteIpConfigPath.erb"
+	# Pick the template path we're going to use
+	$mod_path = get_module_path('apache2')
+	$specific = "$mod_path/templates/$operatingsystem/$operatingsystemrelease$params::remoteIpConfigPath.erb"
+	$default  = "$mod_path/templates/default$params::remoteIpConfigPath.erb"
 
-        # Set up the config file
-        file { $params::remoteIpConfigPath :
-                owner   => 'root',
-                group   => 'root',
-                content => inline_template( file( $specific, $default ) ),
-                notify  => $apache2::serviceNotify,
-        }
+	# Set up the config file
+	file { $params::remoteIpConfigPath :
+		owner   => 'root',
+		group   => 'root',
+		content => inline_template( file( $specific, $default ) ),
+		notify  => $apache2::serviceNotify,
+		require	=> Package[ $params::packageName ],
+	}
 
 }
--- manifests/statusandinfo.pp
+++ manifests/statusandinfo.pp
@@ -1,9 +1,20 @@
 class apache2::statusAndInfo
+(
+	$serverInfoAccessIp = undef,
+	$serverStatusAccessIp = undef
+)
 {
-	#
+	include apache2::access
+
+	validate_string( $serverInfoAccessIp )
+	validate_string( $serverStatusAccessIp )
+
+	# Enable the modules
+	apache2::enableModule{ 'status': }
+	apache2::enableModule{ 'info': }
+
 	# Simply put in the default server-status and server-info files,
 	# but using the new-style access control configurations
-	#
 	file { $params::statusConfigPath :
 		ensure	=> present,
 		source	=> "puppet:///modules/apache2${params::statusConfigPath}",
@@ -15,9 +26,17 @@ class apache2::statusAndInfo
 		require	=> Package[ $params::packageName ],
 	}
 
-	#
-	# Enable the modules
-	#
-	apache2::enableModule{ 'status': }
-	apache2::enableModule{ 'info': }
+	# Set up access
+	if ( is_ip_address( $serverInfoAccessIp ) ) {
+        	apache2::grantAccessToIp { 'server-info' :
+                	location        => '/server-info',
+                	ip              => $serverInfoAccessIp,
+        	}
+	}
+	if ( is_ip_address( $serverInfoAccessIp ) ) {
+        	apache2::grantAccessToIp { 'server-status' :
+                	location        => '/server-status',
+                	ip              => $serverStatusAccessIp,
+        	}
+	}
 }
