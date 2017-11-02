f5fbf22b0b131a19fc5f22e7612c851e705f5f89
Tim McLaughlin <mclaugtd@wwu.edu>
2017-11-02 09:46:09 -0700

    Adding custom fact containing already-enabed modules.

---
+++ lib/facter/apache2_mods.rb
@@ -0,0 +1,8 @@
+Facter.add("apache2_mods") do
+  confine :operatingsystem => "OpenSuSE"
+  setcode do
+    mods_string = `grep '^APACHE_MODULES' /etc/sysconfig/apache2 | awk -F\" '{print $2;}'`
+    mods = mods_string.split(' ')
+    mods
+  end
+end
