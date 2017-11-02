b3667a4c4baaf15aeb945eb8b5f40b72c38dda2b
Tim McLaughlin <mclaugtd@wwu.edu>
2017-11-02 09:58:22 -0700

    Fixing bug - need extra backslash to make things happy.

--- lib/facter/apache2_mods.rb
+++ lib/facter/apache2_mods.rb
@@ -1,7 +1,7 @@
 Facter.add("apache2_mods") do
   confine :operatingsystem => "OpenSuSE"
   setcode do
-    mods_string = `grep '^APACHE_MODULES' /etc/sysconfig/apache2 | awk -F\" '{print $2;}'`
+    mods_string = `grep '^APACHE_MODULES' /etc/sysconfig/apache2 | awk -F\\" '{print $2;}'`
     mods = mods_string.split(' ')
     mods
   end
